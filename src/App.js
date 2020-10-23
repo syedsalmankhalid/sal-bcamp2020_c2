07f52b191785fa8b88437819a0e9bab92f76ec24
syedsalmankhalid <syedsalmankhalid@yahoo.com>
2020-10-23 17:07:17 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">Learn React-Syed 
-  Salman</div>;
+  return <div className="App">Learn React-Syed Salman Khalid</div>;
 }
 
 export default App;
