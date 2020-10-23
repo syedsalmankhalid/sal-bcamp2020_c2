a4a0daecaa9be45503667474846323ca2568f247
syedsalmankhalid <syedsalmankhalid@yahoo.com>
2020-10-23 19:40:22 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Learn React-Syed Salman Khalid</div>;
+  return (
+    <div className="App">
+      <hi>Learn React-Syed Salman Khalid, from Karachi</hi>
+    </div>
+  );
 }
 
 export default App;
